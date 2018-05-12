9ac807f1b95fe36beffb983299d3d7e17cfed9de
Nahin <tt@gmail.com>
2018-05-11 20:32:25 -0400

    "Nahin Two"

--- project1/src/project1/FirstGit.java
+++ project1/src/project1/FirstGit.java
@@ -5,14 +5,14 @@ public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("Test 1");
 		System.out.println("ben");
-<<<<<<< HEAD
+
 		System.out.println("Nahin");
-=======
+
 		System.out.println("push it real good");
 		System.out.println("ben");
 System.out.println("Juel");
 		System.out.println("saymon");
->>>>>>> branch 'master' of https://github.com/talentek/project1.git
+
 
 
 	}
