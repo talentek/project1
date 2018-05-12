b5c104e771443efdfe0830113f5a1a6bf72e8966
Gurpreet Singh <gsingh573@gmail.com>
2018-05-11 20:54:19 -0400

    fixed

--- project1/src/project1/FirstGit.java
+++ project1/src/project1/FirstGit.java
@@ -2,62 +2,49 @@ package project1;
 
 public class FirstGit {
 
-	public static void main(String args) {
+	public static void main(String[] args) {
 		System.out.println("Test 1");
 
 		System.out.println("ben");
-System.out.println("Jay");
+		System.out.println("Jay");
 
 		System.out.println("Avin here");
 
-
 		System.out.println("ben");
-System.out.println("Jay Hasan");
+		System.out.println("Jay Hasan");
 
 		System.out.println("ben");
 		System.out.println("singisdjggakjhjkhkjhkjhjhjkhgdsagadsdafkldsjglajgh");
 
-
 		System.out.println("ben");
 		System.out.println("Omeko!");
-	
-
 
 		System.out.println("Future Automation Engineers");
 
-
 		System.out.println("Nahin");
 
-		
 		System.out.println("Rihanna");
 
 		System.out.println("Avin here");
 
-
 		System.out.println("push it real good");
 
 		System.out.println("ben");
-System.out.println("Juel");
+		System.out.println("Juel");
 		System.out.println("saymon");
 
-
-	
 		System.out.println("Juel");
 
 		System.out.println("saymon");
-		
+
 		System.out.println("Nahin is gucci");
-		
+
 		System.out.println("America runs on Dunkin Donuts");
-
-
 
 		System.out.println("saymon");
 
-
 	}
-//username=talentek
-//pass ttjob123//
-
+	// username=talentek
+	// pass ttjob123//
 
 }
